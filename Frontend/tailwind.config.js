f510721e3c82ed5c3dd8243a9a623de0e6de690d
Hema Bindu <binduh772@gmail.com>
2024-10-10 10:57:12 +0530

    #start project

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // This ensures Tailwind works with Angular's templates and components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
